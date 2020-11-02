763fd4a957551ffaffc8200aaec0eac4ecf8540f
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-11-02 18:58:36 +0800

    Run a ticker alongside the GUI
    Fixme:
    - make this optional
    - let the ticker communicate with the GUI

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 #include <mc_rtc/version.h>
 
+#include <mc_control/mc_global_controller.h>
+
 #include <cmath>
 #include <iostream>
 
@@ -20,14 +22,23 @@
 #  include <emscripten/emscripten.h>
 #endif
 
-#include "RobotModel.h"
+// Target FPS for the ticker
+constexpr size_t fps = 100;
 
 static OrbitCamera * camera_ptr = nullptr;
 static Client * client_ptr = nullptr;
 static SceneState * state_ptr = nullptr;
 static ImGuiIO * io_ptr = nullptr;
-static mc_rbdyn::Robot * robot_ptr = nullptr;
-static RobotModel * model_ptr = nullptr;
+
+struct TickerLoopData
+{
+  size_t iter = 0;
+  mc_control::MCGlobalController * gc = nullptr;
+  std::function<void()> simulateSensors;
+  std::chrono::system_clock::time_point start_t;
+};
+
+static TickerLoopData data;
 
 void RenderLoop()
 {
@@ -35,8 +46,6 @@ void RenderLoop()
   static auto & client = *client_ptr;
   static auto & state = *state_ptr;
   static auto & io = *io_ptr;
-  static auto & robot = *robot_ptr;
-  static auto & model = *model_ptr;
   ImGui_ImplOpenGL3_NewFrame();
   ImGui_ImplRaylib_NewFrame();
   ImGui::NewFrame();
@@ -53,7 +62,6 @@ void RenderLoop()
 
   client.update(state);
   camera.update(state);
-  model.update(robot);
   //----------------------------------------------------------------------------------
 
   // Draw
@@ -69,8 +77,6 @@ void RenderLoop()
   client.draw3D(camera);
 
   DrawFrame(sva::PTransformd::Identity());
-
-  model.draw(camera);
 
   EndMode3D();
 
@@ -134,12 +140,64 @@ int main(void)
 
   mc_rtc::log::info("mc_rtc::version() {}", mc_rtc::version());
 
-  auto rm = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
-  auto robots = mc_rbdyn::loadRobot(*rm);
-  auto & robot = robots->robot();
-  robot_ptr = &robot;
-  RobotModel model(robot);
-  model_ptr = &model;
+  // auto rm = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  // mc_rtc::log::info("Creating global controller");
+  // mc_control::MCGlobalController::GlobalConfiguration gconf("/assets/etc/mc_rtc.yaml", rm);
+  mc_control::MCGlobalController gc("/assets/etc/mc_rtc.yaml");
+
+  const auto & mb = gc.robot().mb();
+  const auto & mbc = gc.robot().mbc();
+  const auto & rjo = gc.ref_joint_order();
+  std::vector<double> initq;
+  for(const auto & jn : rjo)
+  {
+    for(const auto & qi : mbc.q[static_cast<unsigned int>(mb.jointIndexByName(jn))])
+    {
+      initq.push_back(qi);
+    }
+  }
+
+  std::vector<double> qEnc(initq.size(), 0);
+  std::vector<double> alphaEnc(initq.size(), 0);
+  auto simulateSensors = [&, qEnc, alphaEnc]() mutable {
+    auto & robot = gc.robot();
+    for(unsigned i = 0; i < robot.refJointOrder().size(); i++)
+    {
+      auto jIdx = robot.jointIndexInMBC(i);
+      if(jIdx != -1)
+      {
+        auto jointIndex = static_cast<unsigned>(jIdx);
+        qEnc[i] = robot.mbc().q[jointIndex][0];
+        alphaEnc[i] = robot.mbc().alpha[jointIndex][0];
+      }
+    }
+    gc.setEncoderValues(qEnc);
+    gc.setEncoderVelocities(alphaEnc);
+    gc.setSensorPositions({{"FloatingBase", robot.posW().translation()}});
+    gc.setSensorOrientations({{"FloatingBase", Eigen::Quaterniond{robot.posW().rotation()}}});
+  };
+  data.simulateSensors = simulateSensors;
+
+  gc.setEncoderValues(qEnc);
+  gc.init(initq, gc.robot().module().default_attitude());
+  gc.running = true;
+  data.start_t = std::chrono::system_clock::now();
+  data.gc = &gc;
+
+  std::thread th([]() {
+    auto loop_fn = [](void * dataPtr) {
+      auto & data = *static_cast<TickerLoopData *>(dataPtr);
+      data.simulateSensors();
+      data.gc->run();
+      if(++data.iter % fps == 0)
+      {
+        using duration_ms = std::chrono::duration<double, std::milli>;
+        double dt = duration_ms(std::chrono::system_clock::now() - data.start_t).count();
+        mc_rtc::log::info("Controller running for {} seconds, real-time: {}", data.iter / fps, dt / 1000.0);
+      }
+    };
+    emscripten_set_main_loop_arg(loop_fn, &data, fps, 1);
+  });
 
 #ifdef __EMSCRIPTEN__
   emscripten_set_main_loop(RenderLoop, 0, 1);
