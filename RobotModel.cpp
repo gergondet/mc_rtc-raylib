efe90aab9fe35ce93f04fc4f432bc2466a0fc733
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-11-02 18:43:48 +0800

    [web] Support gooch shading

--- RobotModel.cpp
+++ RobotModel.cpp
@@ -14,6 +14,66 @@
 #include <boost/filesystem.hpp>
 namespace bfs = boost::filesystem;
 
+std::string GOOSH_VERTEX_SHADER_ES = R"(
+#version 100
+
+// Input vertex attributes
+attribute vec3 vertexPosition;
+attribute vec2 vertexTexCoord;
+attribute vec3 vertexNormal;
+attribute vec4 vertexColor;
+
+// Input uniform values
+uniform mat4 mvp;
+uniform mat4 matModel;
+
+// Output vertex attributes (to fragment shader)
+varying vec3 fragPosition;
+varying vec2 fragTexCoord;
+varying vec4 fragColor;
+varying vec3 fragNormal;
+
+// https://github.com/glslify/glsl-inverse
+mat3 inverse(mat3 m)
+{
+  float a00 = m[0][0], a01 = m[0][1], a02 = m[0][2];
+  float a10 = m[1][0], a11 = m[1][1], a12 = m[1][2];
+  float a20 = m[2][0], a21 = m[2][1], a22 = m[2][2];
+
+  float b01 = a22*a11 - a12*a21;
+  float b11 = -a22*a10 + a12*a20;
+  float b21 = a21*a10 - a11*a20;
+
+  float det = a00*b01 + a01*b11 + a02*b21;
+
+  return mat3(b01, (-a22*a01 + a02*a21), (a12*a01 - a02*a11),
+              b11, (a22*a00 - a02*a20), (-a12*a00 + a02*a10),
+              b21, (-a21*a00 + a01*a20), (a11*a00 - a01*a10))/det;
+}
+
+// https://github.com/glslify/glsl-transpose
+mat3 transpose(mat3 m)
+{
+  return mat3(m[0][0], m[1][0], m[2][0],
+              m[0][1], m[1][1], m[2][1],
+              m[0][2], m[1][2], m[2][2]);
+}
+
+void main()
+{
+    // Send vertex attributes to fragment shader
+    fragPosition = vec3(matModel*vec4(vertexPosition, 1.0));
+    fragTexCoord = vertexTexCoord;
+    fragColor = vertexColor;
+
+    mat3 normalMatrix = transpose(inverse(mat3(matModel)));
+    fragNormal = normalize(normalMatrix*vertexNormal);
+
+    // Calculate final vertex position
+    gl_Position = mvp*vec4(vertexPosition, 1.0);
+}
+)";
+
 std::string GOOSH_VERTEX_SHADER = R"(
 #version 330
 
@@ -49,6 +109,62 @@ void main()
 )";
 
 // Fragment shader from https://rendermeapangolin.wordpress.com/2015/05/07/gooch-shading/
+
+std::string GOOSH_FRAGMENT_SHADER_ES = R"(
+#version 100
+
+precision mediump float;
+
+// Vertex attributes
+varying vec3 fragPosition;
+varying vec2 fragTexCoord;
+varying vec4 fragColor;
+varying vec3 fragNormal;
+
+// Input uniform values
+uniform sampler2D texture0;
+uniform vec4 colDiffuse;
+uniform vec3 viewPos;
+
+vec4 gooch_shading(vec4 m_color, //color of the mesh
+                   float m_shine, //shininess of the surface
+                   vec3 l_direction, //light direction
+                   vec3 v_normal, //normal
+                   vec3 c_direction) //camera direction
+{
+    //diffuse
+    float kd = 1.0;
+    float a = 0.2;
+    float b = 0.6;
+
+    float NL = dot(normalize(v_normal), normalize(l_direction));
+
+    float it = ((1.0 + NL) / 2.0);
+    vec3 color = (1.0-it) * (vec3(0.0, 0.0, 0.4) + a*m_color.xyz)
+               +  it * (vec3(0.4, 0.4, 0.0) + b*m_color.xyz);
+
+    //Highlights
+    vec3 R = reflect( -normalize(l_direction),
+                      normalize(v_normal) );
+    float ER = clamp( dot( normalize(c_direction),
+                           normalize(R)),
+                     0.0, 1.0);
+
+    vec4 spec = vec4(1.0) * pow(ER, m_shine);
+
+    return vec4(color+spec.xyz, m_color.a);
+}
+
+void main()
+{
+  gl_FragColor = texture2D(texture0, fragTexCoord) * colDiffuse * fragColor;
+  gl_FragColor = gooch_shading(gl_FragColor,
+                               0.5,
+                               vec3(0, 0, 1),
+                               fragNormal,
+                               normalize(viewPos));
+}
+)";
 
 std::string GOOSH_FRAGMENT_SHADER = R"(
 #version 330
@@ -143,7 +259,7 @@ BodyDrawer::ModelData::~ModelData()
   UnloadModel(model);
 }
 
-BodyDrawer::BodyDrawer(const std::vector<rbd::parsers::Visual> & v, Shader  * shader)
+BodyDrawer::BodyDrawer(const std::vector<rbd::parsers::Visual> & v, Shader * shader)
 {
   auto fromMesh = [&](const rbd::parsers::Visual & v) {
     const auto & mesh = boost::get<rbd::parsers::Geometry::Mesh>(v.geometry.data);
@@ -205,9 +321,11 @@ RobotModel::RobotModel(const mc_rbdyn::Robot & robot, bool useCollisionModel)
   const auto & visual = useCollisionModel ? robot.module()._collision : robot.module()._visual;
 #ifndef __EMSCRIPTEN__
   shader_ = LoadShaderCode(GOOSH_VERTEX_SHADER.c_str(), GOOSH_FRAGMENT_SHADER.c_str());
+#else
+  shader_ = LoadShaderCode(GOOSH_VERTEX_SHADER_ES.c_str(), GOOSH_FRAGMENT_SHADER_ES.c_str());
+#endif
   shader_.locs[LOC_MATRIX_MODEL] = GetShaderLocation(shader_, "matModel");
   shader_.locs[LOC_VECTOR_VIEW] = GetShaderLocation(shader_, "viewPos");
-#endif
   for(const auto & b : robot.mb().bodies())
   {
     if(!visual.count(b.name()))
@@ -216,11 +334,7 @@ RobotModel::RobotModel(const mc_rbdyn::Robot & robot, bool useCollisionModel)
     }
     else
     {
-#ifndef __EMSCRIPTEN__
       bodies_.emplace_back(visual.at(b.name()), &shader_);
-#else
-      bodies_.emplace_back(visual.at(b.name()));
-#endif
     }
   }
 }
@@ -237,9 +351,7 @@ void RobotModel::update(const mc_rbdyn::Robot & robot)
 void RobotModel::draw(Camera camera)
 {
   float cameraPos[3] = {camera.position.x, camera.position.y, camera.position.z};
-#ifndef __EMSCRIPTEN__
   SetShaderValue(shader_, shader_.locs[LOC_VECTOR_VIEW], cameraPos, UNIFORM_VEC3);
-#endif
   for(auto & b : bodies_)
   {
     b.draw();
